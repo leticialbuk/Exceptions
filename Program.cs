9b52625797abfa4ee1b89e1ebd3d97eb45a25db1
leticialbuk <leticia.albuk@gmail.com>
2023-03-30 17:42:30 -0300

    Usando finally para fechar as try abertas

--- Program.cs
+++ Program.cs
@@ -10,20 +10,46 @@ namespace M
 
             try
             {
-                for (var index = 0; index < 10; index++)
-                {
-                    Console.WriteLine(arr[index]);
-                }
-
+                Cadastrar("");
             }
-            catch (Exception ex)
+            catch (IndexOutOfRangeException ex)
             {
-                Console.Clear();
                 Console.WriteLine(ex.InnerException);
                 Console.WriteLine(ex.Message);
-                Console.WriteLine("Ops, algo deu errado!");
+                Console.WriteLine("Indice não encontrado");
             }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Falha ao cadastrar texto!");
+            }
+            catch (MinhaException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.QuandoAconteceu);
+                Console.WriteLine("Exceção customizada");
+            }
+            finally
+            {
+                Console.WriteLine("Chegou ao fim :)");
+            }
+        }
 
+        private static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new MinhaException(DateTime.Now);
+        }
+
+        public class MinhaException : Exception
+        {
+            public MinhaException(DateTime date)
+            {
+                QuandoAconteceu = date;
+            }
+            public DateTime QuandoAconteceu { get; set; }
         }
     }
 }
